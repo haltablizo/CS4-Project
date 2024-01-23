5fbf82922eb106345b045a7d696a61b0f85950ff
hannah angela <hannah angela@your-mom>
2024-01-23 09:36:13 +0800

    Added fields

--- src/arachne/Player.java
+++ src/arachne/Player.java
@@ -1,13 +1,16 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package arachne;
 
-/**
- *
- * @author GRAVITON
- */
 public class Player {
+    private String name; 
+    private float popMeter = 0; 
+    private int atk = 10; 
+    private int def = 7; 
+    private int hp; 
+    private int maxHp = 20; 
     
+    public Player(String name, int hp) {
+        name = this.name; 
+        hp = maxHp; 
+    }   
 }
